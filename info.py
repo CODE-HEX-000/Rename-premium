151c39b7476102800f3a82cc873687f29f665bac
DEVIL BOTZ <110474610+Devil-Botz@users.noreply.github.com>
2023-01-05 22:31:34 +0530

    Update info.py

--- info.py
+++ info.py
@@ -10,3 +10,7 @@ API_HASH = os.environ.get("API_HASH", "d3d1e14cf654404daee47bc48eb01c8d")
 STRING = os.environ.get("STRING", "BQELehwAqPeQ4BoKidLvVVDUqaBwg8Y9dH6oGdlwbW24d9g0rXFqgM5sIgW1V6EAgy7oWwNggbL9-eUqjXadGTHtSNdRkP7XJ0Eb1CfXStnBZKlAgUKOxgWVV-VOmKSLR0sFTMt823KWqqlzkmqXepLAh1BIT-K2Sucew8EY9m0Id_Ajkzu-xadS0ZypowuMNkcljkSCdZkB3vlVa9VGLte_NmBi99WbJ_-3xhyXf26OV4m-8ovrk2Cgi_mJLCCTDevUOIN2dYNfIkRvj1MRJEuQdzFGZOeE_1O8zcVqEVeP81ob6Zuzwelv4uOdrjV00JwGyHO1Mq0jk1ERNfIybsFS34eBZwAAAABa8qsxAA")
 
 ADMIN = int(os.environ.get("ADMIN", 1525852977))
+
+DB_NAME = os.environ.get("DB_NAME","hellodb")
+
+DB_URL = os.environ.get("DB_URL","mongodb+srv://hellodb:hellodb@cluster0.nmnwvqm.mongodb.net/?retryWrites=true&w=majority")
